7263f0a2931ac0d8676dc9425609607eede6438b
RKJ <jrohit1110@gmail.com>
2023-09-06 16:29:42 +0530

    Assembly Code compilation keybindings

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -16,20 +16,47 @@ local function _debug_print(input)
   end
 end
 
-local function c_cmd(optimization_level, filename)
-  vim.cmd(":silent !cd %:p:h && clang -S -mllvm --x86-asm-syntax=intel -" .. optimization_level .. " " .. filename)
+local function cpp_cmd(optimization_level, filename)
+  local bin = vim.fn.fnamemodify(filename, ":h") .. "/bin" -- folder path + \bin
+  local fln = vim.fn.fnamemodify(filename, ":t")           -- only filename with extesion
+  _debug_print(bin)
+  _debug_print("is idr : " .. vim.fn.isdirectory(bin))     -- Test: Check if bin exists
+
+  local cmd = ""
+  if DEBUG then
+    cmd = ":!cd " .. vim.fn.fnamemodify(filename, ":h") .. " &&"        -- First goto the fodler where main.cpp is loacted
+  else
+    cmd = ":silent !cd " .. vim.fn.fnamemodify(filename, ":h") .. " &&" -- First goto the fodler where main.cpp is loacted
+  end
+
+  -- Create bin if not exists
+  if vim.fn.isdirectory(bin) == 1 then
+    cmd = cmd
+    _debug_print("found bin")
+  else
+    cmd = cmd .. " mkdir -p bin &&"
+    _debug_print("not found bin")
+  end
+
+  cmd = cmd .. " cd bin &&"                                -- goto bin
+  cmd = cmd .. " clang -S -mllvm --x86-asm-syntax=intel -" -- produce assembly file inside bin
+  cmd = cmd .. optimization_level .. " ../" .. fln         -- reference outside filename from within the bin
+  _debug_print(cmd)
+  vim.cmd(cmd)
 end
 
 function AsmOutputUpdate(optimization_level)
   if optimization_level == nil then
     optimization_level = "O0"
   end
+
   local filename = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(0))
   _debug_print(filename)
-  c_cmd(optimization_level, filename)
   local output_file = vim.fn.fnamemodify(filename, ":t:r") .. ".s"
   _debug_print(output_file)
-  -- vim.fn.system(cmd)
+
+  cpp_cmd(optimization_level, filename)
+
   local found = false
   -- Iterate through all open buffers
   for _, winnr in ipairs(vim.api.nvim_list_wins()) do
@@ -45,8 +72,9 @@ function AsmOutputUpdate(optimization_level)
   end
   if not found then
     _debug_print("not found")
-    vim.cmd(":vs " .. vim.fn.fnamemodify(filename, ":r") .. ".s")
-    vim.cmd("w")
+    -- Open the <filename>.s file from the bin directory
+    vim.cmd(":vs " .. vim.fn.fnamemodify(filename, ":h") .. "/bin/" .. vim.fn.fnamemodify(filename, ":t:r") .. ".s")
+    vim.cmd(":wincmd p")
   end
   -- Delete_scratch_buffers()
 end
