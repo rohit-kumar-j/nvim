c0b72fdd1631aa72cf48b32d3c86c19f6194c456
RKJ <jrohit1110@gmail.com>
2023-09-09 03:20:42 +0530

    Added neogen for docstring generation

--- lua/core/plugin_config/neogen.lua
+++ lua/core/plugin_config/neogen.lua
@@ -1,5 +1,8 @@
 return {
   "danymat/neogen",
+  keys = {
+    { "<leader>N", ":lua require('neogen').generate()<CR>", desc = "Generate Doc String" }
+  },
   dependencies = "nvim-treesitter/nvim-treesitter",
   config = function()
     require("neogen").setup({ snippet_engine = "luasnip" })
